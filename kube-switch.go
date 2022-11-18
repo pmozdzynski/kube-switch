9bf4307ced99c43a8885517c5496ef8e8ac110b2
Piotr Mozdzynski <piotr.mozdzynski@williamhill.com>
2022-11-18 10:27:58 +0100

    adding current-context detection

--- kube-switch.go
+++ kube-switch.go
@@ -15,18 +15,29 @@ func main() {
 		Use:   "kube-switch",
 		Short: "Interactive Kubernetes context switcher",
 		Run: func(cmd *cobra.Command, args []string) {
+			// Get the current context
+			currentContext, err := getCurrentContext()
+			if err != nil {
+				fmt.Printf("Error fetching current context: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Current context: %s\n", currentContext)
+
+			// Get the list of contexts
 			contexts, err := getContexts()
 			if err != nil {
 				fmt.Printf("Error fetching contexts: %v\n", err)
 				os.Exit(1)
 			}
 
-			selectedContext, err := promptForContext(contexts)
+			// Prompt the user to select a context
+			selectedContext, err := promptForContext(contexts, currentContext)
 			if err != nil {
 				fmt.Printf("Error selecting context: %v\n", err)
 				os.Exit(1)
 			}
 
+			// Switch to the selected context
 			err = switchContext(selectedContext)
 			if err != nil {
 				fmt.Printf("Error switching context: %v\n", err)
@@ -40,6 +51,17 @@ func main() {
 	rootCmd.Execute()
 }
 
+// Fetch the current Kubernetes context
+func getCurrentContext() (string, error) {
+	cmd := exec.Command("kubectl", "config", "current-context")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch current context: %v", err)
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
+// Fetch all Kubernetes contexts
 func getContexts() ([]string, error) {
 	cmd := exec.Command("kubectl", "config", "get-contexts", "-o", "name")
 	output, err := cmd.Output()
@@ -51,12 +73,23 @@ func getContexts() ([]string, error) {
 	return contexts, nil
 }
 
-func promptForContext(contexts []string) (string, error) {
+// Prompt the user to select a Kubernetes context
+func promptForContext(contexts []string, currentContext string) (string, error) {
+	// Mark the current context in the list
+	contextItems := make([]string, len(contexts))
+	for i, context := range contexts {
+		if context == currentContext {
+			contextItems[i] = fmt.Sprintf("%s (current)", context)
+		} else {
+			contextItems[i] = context
+		}
+	}
+
 	prompt := promptui.Select{
 		Label: "Select Kubernetes Context",
-		Items: contexts,
+		Items: contextItems,
 		Searcher: func(input string, index int) bool {
-			context := contexts[index]
+			context := contextItems[index]
 			return strings.Contains(context, input)
 		},
 		StartInSearchMode: true,
@@ -67,9 +100,13 @@ func promptForContext(contexts []string) (string, error) {
 		return "", err
 	}
 
+	// Strip "(current)" from the selected result, if present
+	result = strings.Replace(result, " (current)", "", 1)
+
 	return result, nil
 }
 
+// Switch to the specified Kubernetes context
 func switchContext(context string) error {
 	cmd := exec.Command("kubectl", "config", "use-context", context)
 	output, err := cmd.CombinedOutput()
@@ -79,5 +116,3 @@ func switchContext(context string) error {
 
 	return nil
 }
-
-
